c5fd9520c9fe14ce62b03b8a7b0b22404363f9fe
Mahdi Nedaee <53142654+mhdned@users.noreply.github.com>
2025-05-05 12:50:08 +0330

    REFACTOR core

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
-import fs from 'fs';
-import path from 'path';
-import { Request, Response, NextFunction } from 'express';
-
-import { MetriuoOptions, RequestDataType } from './metriuo.types';
 import { MetriuoOptionsType } from './types/monitoring.type';
+import { requestLogger } from './middlewares/logger.middleware';
 
-export class Metriuo {
+class Metriuo {
   public static metriuoInstance: Metriuo;
 
   private folder: string;
-  private logFileFormat: string;
+  private logFileFormat: 'json' | 'txt';
 
   private constructor(options: MetriuoOptionsType) {
     this.folder = options.folder;
@@ -21,71 +17,16 @@ export class Metriuo {
     return Metriuo.metriuoInstance;
   }
 
-  public static monitoring() {
+  public monitoring() {
     return;
   }
 
-  public static logger() {
-    return;
-  }
-}
-
-export default function requestLogger(options: MetriuoOptions = {}) {
-  const { logFolder = '/logs', logFormat = 'json' } = options;
-
-  const today: Date = new Date();
-  const todayString: string = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`;
-
-  return (req: Request, res: Response, next: NextFunction) => {
-    const start = process.hrtime();
-
-    res.on('finish', () => {
-      const diff = process.hrtime(start);
-      const responseTime = `${(diff[0] * 1e3 + diff[1] / 1e6).toFixed(3)} ms`;
-
-      const logData: RequestDataType = {
-        url: req.url,
-        host: req.get('host') || '',
-        baseUrl: req.baseUrl,
-        hostname: req.hostname,
-
-        ip: req.ip,
-        ips: req.ips,
-        location: req.headers['location'],
-        userAgent: req.headers['user-agent'],
-        connection: req.headers['connection'],
-        authorization: req.headers['authorization'],
-
-        path: req.path,
-        body: req.body,
-        query: req.query,
-        params: req.params,
-
-        method: req.method,
-        httpVersion: req.httpVersion,
-
-        responseTime,
-        responseStatus: res.statusCode,
-      };
-
-      const logPath = path.join(
-        './',
-        `${logFolder}/${todayString}.${logFormat}`
-      );
-
-      if (!fs.existsSync(logFolder)) {
-        fs.mkdirSync(logFolder, { recursive: true });
-      }
-
-      const logLine = JSON.stringify(logData) + '\n';
-
-      fs.appendFile(logPath, logLine, (error) => {
-        if (error) {
-          console.error('Failed to write log:', error);
-        }
-      });
+  public logger() {
+    return requestLogger({
+      folder: this.folder,
+      logFileFormat: this.logFileFormat,
     });
-
-    next();
-  };
+  }
 }
+
+module.exports = Metriuo;
