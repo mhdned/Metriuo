2dda220748e4b63f49ee8789110c3f8241f199f4
Mahdi Nedaee <53142654+mhdned@users.noreply.github.com>
2025-05-07 18:01:01 +0330

    CHANGE name of setup method to initialize ADD DockDB to handle request logs information

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import * as path from 'path';
+import * as fs from 'fs/promises';
+import { DuckDBConnection, DuckDBInstance } from '@duckdb/node-api';
+
 import { MetriuoOptionsType } from './types/monitoring.type';
 import { requestLogger } from './middlewares/logger.middleware';
 
@@ -6,15 +10,34 @@ class Metriuo {
 
   private folder: string;
   private logFileFormat: 'json' | 'txt';
+  private static duckDBInstance: DuckDBInstance;
+  private static duckDBConnection: DuckDBConnection;
 
   private constructor(options: MetriuoOptionsType) {
     this.folder = options.folder;
     this.logFileFormat = options.logFileFormat ?? 'json';
   }
 
-  public static setup(options: MetriuoOptionsType): Metriuo {
+  public static async initialize(
+    options: MetriuoOptionsType
+  ): Promise<Metriuo> {
     Metriuo.metriuoInstance = new Metriuo(options);
+    await this.initializeDuckDB();
     return Metriuo.metriuoInstance;
+  }
+
+  private static async initializeDuckDB() {
+    const databasePath = path.join(__dirname, './database/metriuo.db');
+    const instance = await DuckDBInstance.create(databasePath);
+    const sqlFilePath = path.join(
+      __dirname,
+      './database/sql/createRequestLogTable.sql'
+    );
+    const createRequestTableQuery = await fs.readFile(sqlFilePath, 'utf-8');
+    const connection = await instance.connect();
+    await connection.run(createRequestTableQuery);
+    this.duckDBConnection = connection;
+    this.duckDBInstance = instance;
   }
 
   public monitoring() {
