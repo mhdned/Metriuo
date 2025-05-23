c0aef6bdc41d4dc81a54ffd2e6449dec6dbd6097
Mahdi Nedaee <53142654+mhdned@users.noreply.github.com>
2025-05-23 18:49:44 +0330

    FEATURE monitoring page

--- src/middlewares/monitoring.middleware.ts
+++ src/middlewares/monitoring.middleware.ts
@@ -1,13 +1,28 @@
+import ejs from 'ejs';
+import path from 'path';
 import { Request, Response, NextFunction } from 'express';
 
-async function monitoringView() {
-  return (req: Request, res: Response, next: NextFunction) => {
-    // Read json file information from a singleton class
+import { DuckDatabaseService } from './../services/duckdb.service';
 
-    // Render the template engine
+const viewPath = path.join(__dirname, '../views/monitoring.ejs');
 
-    // Finish job
+export async function monitoringView(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  // Read json file information from a singleton class
+  const db = await DuckDatabaseService.createInstance();
+  const requestLogInformations = await db.read(0, 10);
 
-    next();
-  };
+  // Render the template engine
+
+  // Finish job
+  ejs.renderFile(viewPath, { logs: requestLogInformations }, (error, html) => {
+    if (error) {
+      console.error('EJS render error:', error);
+      return res.status(500).send('Internal Server Error');
+    }
+    return res.send(html);
+  });
 }
